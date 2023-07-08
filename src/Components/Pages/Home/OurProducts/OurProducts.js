3efff4737649b8889ece1d73182be7c8ee05ee5e
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-07-09 01:18:49 +0600

    add products

--- src/Components/Pages/Home/OurProducts/OurProducts.js
+++ src/Components/Pages/Home/OurProducts/OurProducts.js
@@ -4,12 +4,13 @@ import CustomerReview from './CustomerReview';
 const OurProducts = () => {
   return (
     <div className="mx-20 mt-10">
+      <h1 className="my-5 text-5xl font-bold text-center">Our Products</h1>
       <div className="flex gap-20">
         <div>
           <CustomerReview />
         </div>
         <div className="bg-red-500 w-full">
-          <h1>Product</h1>
+          <h1>Product 2</h1>
         </div>
       </div>
       <h1>Products</h1>
