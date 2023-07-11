82b34f7de9825ea2439b2828c8ecb4a8989cc197
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-07-11 16:39:41 +0600

    edit dashboard

--- src/Components/Pages/Dashboard/Dashboard.js
+++ src/Components/Pages/Dashboard/Dashboard.js
@@ -1,37 +1,15 @@
 import React, { useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { AiOutlineHeart, AiOutlineUser } from 'react-icons/ai';
+import { AiOutlineUser } from 'react-icons/ai';
 import { BiEdit } from 'react-icons/bi';
-import { FiFolder, FiMessageSquare, FiShoppingCart } from 'react-icons/fi';
-import { RiSettings4Line } from 'react-icons/ri';
-import { TbReportAnalytics } from 'react-icons/tb';
 import { Link, Outlet } from 'react-router-dom';
 import auth from '../../../firebase.init';
 
 const Dashboard = () => {
   const [user] = useAuthState(auth);
   console.log(user.email);
-  const email = user.email;
-  const menus = [
-    { name: 'Profile', link: '/dashboard', icon: AiOutlineUser },
-    {
-      name: 'Edit Profile',
-      link: '/dashboard/editProfile',
-      icon: BiEdit,
-    },
-    {
-      name: 'My Booking ',
-      link: '/dashboard/editProfile',
-      icon: FiMessageSquare,
-    },
-    { name: 'analytics', link: '/', icon: TbReportAnalytics },
-    { name: 'File Manager', link: '/', icon: FiFolder },
-    { name: 'Cart', link: '/', icon: FiShoppingCart },
-    { name: 'Saved', link: '/', icon: AiOutlineHeart },
-    { name: 'Setting', link: '/', icon: RiSettings4Line },
-  ];
 
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
   return (
     <div className="bg-white ">
       <div>
@@ -61,8 +39,8 @@ const Dashboard = () => {
                 />
                 <div className="flex gap-x-4 items-center">
                   <img
-                    src="https://p7.hiclipart.com/preview/707/501/155/hardware-accessory-circle-settings.jpg"
-                    className={`cursor-pointer duration-500 ${
+                    src="https://www.svgrepo.com/download/7869/settings.svg"
+                    className={`cursor-pointer duration-500 rounded-full ${
                       open && 'rotate-[360deg] h-20  '
                     }`}
                     alt=""
@@ -70,31 +48,6 @@ const Dashboard = () => {
                 </div>
 
                 <div className="mt-4 flex flex-col gap-4 relative">
-                  {/* {menus?.map((menu, i) => (
-                    <Link
-                      to={menu?.link}
-                      key={i}
-                      className={` ${
-                        menu?.margin && 'mt-5'
-                      } group flex items-center text-xl  gap-3.5 font-medium p-2 hover:bg-primary rounded-md`}
-                    >
-                      <div>
-                        {React.createElement(menu?.icon, { size: '20' })}
-                      </div>
-                      <h2
-                        style={{
-                          transitionDelay: `${i + 3}00ms`,
-                        }}
-                        className={`whitespace-pre duration-500 ${
-                          !open && 'opacity-0 translate-x-28 overflow-hidden '
-                        }`}
-                      >
-                        {menu?.name}
-                      </h2>
-                      
-                    </Link>
-                  ))} */}
-
                   <Link
                     to="/dashboard"
                     className={`  group flex items-center text-xl  gap-3.5 font-medium p-2 hover:bg-primary rounded-md`}
