22b2202fdbc006e908b2239143c39cdbb2caf688
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-07-12 21:13:28 +0600

    add my booking

--- src/Components/Pages/Dashboard/My Booking/MyBooking.js
+++ src/Components/Pages/Dashboard/My Booking/MyBooking.js
@@ -1,75 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import auth from '../../../../firebase.init';
-import MyBooking from './MyBooking';
+import React from 'react';
 
-const MyBookings = () => {
-  const [user] = useAuthState(auth);
-  const email = user?.email;
-  const [buys, setBuys] = useState([]);
-  const navigate = useNavigate();
-  useEffect(() => {
-    fetch(`http://localhost:5000/buyBlood/${email}`)
-      .then(res => res.json())
-      .then(data => setBuys(data));
-  }, [buys, email]);
-
-  const handlePayment = id => {
-    navigate(`/payment/${id}`);
-  };
-
-  const handleRemove = id => {
-    const proceed = window.confirm('Are You Sure ?');
-    if (proceed) {
-      const url = `http://localhost:5000/buyBlood/${id}`;
-      fetch(url, {
-        method: 'DELETE',
-      })
-        .then(res => res.json())
-        .then(data => {
-          const remaining = buys.filter(product => product._id !== id);
-          setBuys(remaining);
-          toast.success('Successfully Delete');
-        });
-    }
-  };
+const MyBooking = ({ buy, index, handleRemove, handlePayment }) => {
   return (
-    <div>
-      <div className="overflow-x-auto">
-        <table className="table w-full">
-          {/* head */}
-          <thead>
-            <tr className="text-center">
-              <th className="text-xl">Index</th>
-              <th className="text-xl">Name</th>
-              <th className="text-xl">Student Id</th>
-              <th className="text-xl">Blood Group</th>
-              <th className="text-xl">Quantity</th>
-              <th className="text-xl">Total Price</th>
-              <th className="text-xl">Date</th>
-              <th className="text-xl">Phone</th>
-              <th className="text-xl">Address</th>
-              <th className="text-xl">Payment</th>
-              <th className="text-xl">Remove</th>
-            </tr>
-          </thead>
-          <tbody className="text-center">
-            {buys.map((buy, index) => (
-              <MyBooking
-                key={buy._id}
-                buy={buy}
-                index={index + 1}
-                handleRemove={handleRemove}
-                handlePayment={handlePayment}
-              ></MyBooking>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <tr>
+      <th>{index}</th>
+      <td>{buy?.user?.name}</td>
+      <td>{buy?.user?.studentId}</td>
+      <td>{buy?.bloodGroup}</td>
+      <td>{buy?.quantity}</td>
+      <td>{buy?.totalPrice}</td>
+      <td>{buy?.date}</td>
+      <td>{buy?.phone}</td>
+      <td>{buy?.address}</td>
+
+      <td>
+        {buy?.status === 'Free' ? (
+          <h1 className="font-extrabold text-blue-600 text-xl"> Free</h1>
+        ) : (
+          <>
+            {' '}
+            {buy?.payment ? (
+              <h1 className="font-extrabold text-orange-600 text-xl">Paid</h1>
+            ) : (
+              <button
+                onClick={() => handlePayment(buy?._id)}
+                className="btn btn-secondary  btn-sm text-white"
+              >
+                Payment
+              </button>
+            )}
+          </>
+        )}
+      </td>
+      <td>
+        {' '}
+        <button
+          onClick={() => handleRemove(buy?._id)}
+          className="btn btn-primary  btn-sm text-white"
+        >
+          Remove
+        </button>
+      </td>
+    </tr>
   );
 };
 
-export default MyBookings;
+export default MyBooking;
