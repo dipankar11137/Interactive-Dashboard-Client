33141469fabef3f38815088a42e087c117c0b24c
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-07-16 12:25:47 +0600

    add chatbot condition

--- src/Components/Pages/Dashboard/My Booking/MyBookings.js
+++ src/Components/Pages/Dashboard/My Booking/MyBookings.js
@@ -11,6 +11,7 @@ const MyBookings = () => {
   const [user] = useAuthState(auth);
   const email = user?.email;
   const [buys, setBuys] = useState([]);
+  const [chat, setChat] = useState(false);
   const navigate = useNavigate();
   useEffect(() => {
     fetch(`http://localhost:5000/buyProduct/${email}`)
@@ -40,34 +41,43 @@ const MyBookings = () => {
   return (
     <div>
       <div className="overflow-x-auto p-2">
-        <div className="flex justify-end ">
-          <div className="chat-bot-style  bottom-0">
-            <button>
-              <img
-                className="w-20 h-20"
-                src="https://icon-library.com/images/iphone-messenger-icon/iphone-messenger-icon-29.jpg"
-                alt=""
-              />
-            </button>
-          </div>
-        </div>
-        <div className=" flex justify-end mb-[20px] mr-11 ">
-          {' '}
-          <div className="flex ">
-            <div className="chat-bot-style flex">
-              <ChatBot />
-            </div>
-            <div className=" ml-72 pt-2 ">
-              <button className="w-10 h-10 chat-cross mt-6 -ml-2">
+        {chat ? (
+          <></>
+        ) : (
+          <div className="flex justify-end ">
+            <div className="chat-bot-style  bottom-0">
+              <button onClick={() => setChat(true)}>
                 <img
-                  className="hover:cursor-pointer"
-                  src="https://static.vecteezy.com/system/resources/previews/018/887/462/original/signs-close-icon-png.png"
+                  className="w-20 h-20"
+                  src="https://icon-library.com/images/iphone-messenger-icon/iphone-messenger-icon-29.jpg"
                   alt=""
                 />
               </button>
             </div>
           </div>
-        </div>
+        )}
+        {chat && (
+          <div className=" flex justify-end mb-[20px] mr-11 ">
+            {' '}
+            <div className="flex ">
+              <div className="chat-bot-style flex">
+                <ChatBot />
+              </div>
+              <div className=" ml-72 pt-2 ">
+                <button
+                  onClick={() => setChat(false)}
+                  className="w-10 h-10 chat-cross mt-6 -ml-2"
+                >
+                  <img
+                    className="hover:cursor-pointer"
+                    src="https://static.vecteezy.com/system/resources/previews/018/887/462/original/signs-close-icon-png.png"
+                    alt=""
+                  />
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
 
         <table className="table w-full ">
           {/* head */}
